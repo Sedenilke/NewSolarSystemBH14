c590d379482627b0d24ef56dba7b4f0b49c68ba6
Sedenilke <sedenilke@gmail.com>
2022-12-18 09:26:50 -0800

    My First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      SolarSystem obj = new SolarSystem();
+      obj.moon = 1;
+      obj.star = 2;
+      obj.sun = 3;
+        System.out.println(obj.moon + " " + obj.star + " " + obj.sun );
+    }
+}
